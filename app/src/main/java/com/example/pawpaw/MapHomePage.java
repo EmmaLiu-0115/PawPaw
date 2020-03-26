9a3d6bb2e4d0a29fd8f09e0de48876898f20f7d7
dbao5 <dbao5@wisc.edu>
2020-03-25 21:27:20 -0500

    maphome

---
+++ app/src/main/java/com/example/pawpaw/MapHomePage.java
@@ -0,0 +1,13 @@
+package com.example.pawpaw;
+
+import androidx.fragment.app.FragmentActivity;
+import android.os.Bundle;
+public class MapHomePage extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
